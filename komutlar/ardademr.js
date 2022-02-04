3f10fb6d937cf38cc5f5dc378cb5c9564a8f2f28
muzaffer65 <79631884+muzaffer65@users.noreply.github.com>
2022-02-04 12:07:39 +0300

    Add files via upload

---
+++ komutlar/ardademr.js
@@ -0,0 +1,14 @@
+const Discord = require('discord.js');
+
+exports.run = async (client, message, args) => {
+// ArdaDemr Youtube Kanalına ABONE ROL BOT ALTYAPISI
+message.channel.send('ArdaDemr - Youtube')
+}
+
+exports.conf = {
+  aliases: []
+};
+
+exports.help = {
+  name : "ardademr"
+};// ArdaDemr Youtube Kanalına ABONE ROL BOT ALTYAPISI
